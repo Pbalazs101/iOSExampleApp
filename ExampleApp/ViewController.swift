d743918f439d0a4ac99b9714e628cdf81cde19dc
Balázs Pintér <pbalazs101@gmail.com>
2024-05-27 14:03:14 +0200

    Initial commit

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Balázs Levente Pintér on 27/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
