b8935c1af90d0d9dc5e1bedfcf5dfc574562101c
Balázs Pintér <pbalazs101@gmail.com>
2024-05-28 13:18:05 +0200

    Card struct created

---
+++ ExampleApp/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  ExampleApp
+//
+//  Created by Balázs Levente Pintér on 28/05/2024.
+//
+
+import Foundation
+
+struct Card {
+    
+    var id:Int
+    var title:String
+    var description:String
+}
