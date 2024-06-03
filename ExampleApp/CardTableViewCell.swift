0262d5e48da65ecec97d49348e2753db06780bbd
Balázs Pintér <pbalazs101@gmail.com>
2024-06-03 13:46:27 +0200

    Outlets for labels in CardTableViewCell created

---
+++ ExampleApp/CardTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CardTableViewCell.swift
+//  ExampleApp
+//
+//  Created by Balázs Levente Pintér on 03/06/2024.
+//
+
+import UIKit
+
+class CardTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var arrowImage: UIImageView!
+    
+    
+}
