bcefcad73e2e4db9f5f8b760db67a6e0623aee22
Balázs Pintér <pbalazs101@gmail.com>
2024-06-03 14:00:07 +0200

    Storing data from JSON in Card struct

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cardsTable: UITableView!
     
+    var cardsData: [Card] = []
+    
     func loadCardInfo() {
         
         let url = "http://localhost:8000/CardData.json"
@@ -28,6 +30,9 @@ class ViewController: UIViewController {
             do {
                 let decoder = JSONDecoder()
                 let actualCard = try decoder.decode(Card.self, from: data)
+                self.cardsData.append(Card(id: 0, title: actualCard.title, description: actualCard.description))
+                self.cardsData.append(Card(id: 1, title: actualCard.title, description: actualCard.description))
+                self.cardsData.append(Card(id: 2, title: actualCard.title, description: actualCard.description))
             } catch let error {
                 print(error)
             }
