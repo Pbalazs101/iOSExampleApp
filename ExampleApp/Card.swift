bf853e8d28eeb1e9e06600a645bf93b288acaa97
Balázs Pintér <pbalazs101@gmail.com>
2024-06-24 15:42:39 +0200

    Card.swift now in compliance with conventions

--- ExampleApp/Card.swift
+++ ExampleApp/Card.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-///Representing a single card displayed on the Offers page
+/// Representing a single card displayed on the Offers page
 struct Card: Codable {
-    var id:Int
-    var title:String
-    var description:String
-    var detailedDescription:String
+    var id: Int
+    var title: String
+    var description: String
+    var detailedDescription: String
 }
