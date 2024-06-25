b1a742dac3f421522ff7ac35b2598b58660f96f3
Balázs Pintér <pbalazs101@gmail.com>
2024-06-25 11:37:25 +0200

    Removed unused functions and whitespaces

--- ExampleApp/CustomTableViewCell.swift
+++ ExampleApp/CustomTableViewCell.swift
@@ -9,25 +9,9 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
-    
     @IBOutlet weak var descriptionLabel: UILabel!
-    
-    
     static let identifier = "customCell"
-    
     static func nib() -> UINib {
         return UINib(nibName: "CustomTableViewCell", bundle: nil)
     }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
