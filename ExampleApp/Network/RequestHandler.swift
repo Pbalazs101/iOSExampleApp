a32c975faf6104eee4bda7c45e3e4408155d42a8
Balázs Pintér <pbalazs101@gmail.com>
2024-06-27 11:50:16 +0200

    Removed unused function in RequestHandler.swift

--- ExampleApp/Network/RequestHandler.swift
+++ ExampleApp/Network/RequestHandler.swift
@@ -11,22 +11,6 @@ import Alamofire
 var cardsData: [Card] = []
 
 /// Send an API request to obtain card info from JSON
-func loadCardInfo() {
-    let url = "http://localhost:8000/CardData.json"
-    AF.request(url).response { response in
-        guard let data = response.data else { return }
-        do {
-            let decoder = JSONDecoder()
-            let actualCard = try decoder.decode(Card.self, from: data)
-            cardsData.append(Card(id: 0, title: actualCard.title, description: actualCard.description, detailedDescription: actualCard.detailedDescription))
-            cardsData.append(Card(id: 1, title: actualCard.title, description: actualCard.description, detailedDescription: actualCard.detailedDescription))
-            cardsData.append(Card(id: 2, title: actualCard.title, description: actualCard.description, detailedDescription: actualCard.detailedDescription))
-        } catch let error {
-            print(error)
-        }
-    }
-}
-
 func fetchData(_ completion: @escaping (_ success: Bool, _ data: Data?) -> Void) {
    let url = URL(string: "http://localhost:8000/CardData.json")!
    let task = URLSession.shared.dataTask(with: url) { data, _, _ in
